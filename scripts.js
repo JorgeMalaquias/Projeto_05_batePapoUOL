cd467889f0a623afacecc0a9548e83560e1cc2e5
Jorge <fulano@email.com>
2022-04-18 04:20:20 -0300

    todos requisitos cumpridos

--- scripts.js
+++ scripts.js
@@ -7,13 +7,11 @@ const messageContainer = document.querySelector(".msg-container");
 
 
 function nomeAceito(promise) {
-    console.log("beleza");
     setInterval(conection, 3700);
     atualizarChat();
     setInterval(atualizarChat, 2500);
 }
 function nomeNaoAceito(erro) {
-    console.log("koee");
     user = prompt("Qual seu nome de usuário?");
     userObj = { name: user }
     promise = axios.post("https://mock-api.driven.com.br/api/v6/uol/participants", userObj);
@@ -22,7 +20,6 @@ function nomeNaoAceito(erro) {
 }
 function conection() {
     axios.post("https://mock-api.driven.com.br/api/v6/uol/status", userObj);
-    console.log("online");
 }
 function atualizarChat() {
     promise = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages");
@@ -30,11 +27,36 @@ function atualizarChat() {
 }
 function preencherNaTela(resposta) {
     messageContainer.innerHTML = "";
-    for (let i = 0; i < resposta.data.length; i++) {
-        let message = `<div class="msg ${resposta.data[i].type}">(${resposta.data[i].time}) <strong>${resposta.data[i].from}</strong> para <strong>${resposta.data[i].to}</strong>: ${resposta.data[i].text}</div>`;
+    console.log(resposta.data);
+    let mensagensPermitidas = resposta.data.filter(filtrarReservadas);
+    console.log(mensagensPermitidas);
+    for (let i = 0; i < mensagensPermitidas.length; i++) {
+        let message = `<div class="msg ${mensagensPermitidas[i].type}">(${mensagensPermitidas[i].time}) <strong>${mensagensPermitidas[i].from}</strong> para <strong>${mensagensPermitidas[i].to}</strong>: ${mensagensPermitidas[i].text}</div>`;
         messageContainer.innerHTML += message;
     }
     document.querySelector(".msg:last-child").scrollIntoView();
+}
+
+function filtrarReservadas(array){
+    if((array.type==="private_message") && !(array.to===user)){
+        return false;
+    }
+    return true;
+}
+
+function enviarMsg() {
+    let mensagem = document.querySelector("input").value;
+    document.querySelector("input").value = "";
+    console.log("este é seu value"+ document.querySelector("input").value);
+    let msgObj = {
+        from: user,
+        to: "Todos",
+        text: mensagem,
+        type: "message"
+    }
+    let deuCerto = axios.post("https://mock-api.driven.com.br/api/v6/uol/messages", msgObj);
+    deuCerto.then(atualizarChat);
+    deuCerto.catch(window.location.reload);
 }
 
 user = prompt("Qual seu nome de usuário?");
